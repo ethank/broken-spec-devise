529619f587c4997f4c693336628aa451cb3380e2
Ethan Kaplan <ethankap@murmurs.com>
2011-07-07 21:21:01 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Concertgram2::Application.config.secret_token = 'ae0738434fa9307fc1b3e945f468a9389d225997d3939b7ff4a8949e409da22a4acd4fb370925dd50b1d0bb773e5f411afcf795a01b7a7c299d4470f76be9384'
