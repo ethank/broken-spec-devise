0b47925602644c39ef1afa9a59e3d643fb1d0eec
Ethan Kaplan <ethankap@murmurs.com>
2011-07-07 21:43:46 -0700

    devise and rspec stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,4 +24,22 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   # config.use_transactional_fixtures = true
+
+
+  # ADDED - ERK #
+
+  # Other things
+
+  # Clean up the database
+  require 'database_cleaner'
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.orm = "mongoid"
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end  
+
+
 end
